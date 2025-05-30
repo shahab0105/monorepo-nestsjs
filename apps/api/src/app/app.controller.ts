fc5be14424290e2d7eba23bee65ade8717f139d9
shahabuddinirfan@gmail.com <shahabuddinirfan@gmail.com>
2025-05-30 20:32:01 +0500

    restruct exceptions to gql

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Head } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+// For Render
+  @Head()
+  headData() {
+    return;
   }
 }
--- apps/api/src/common/filters/graphql-exception.filter.ts
+++ apps/api/src/common/filters/graphql-exception.filter.ts
@@ -5,28 +5,36 @@ import {
   HttpException,
   Logger,
 } from '@nestjs/common';
-// import { GqlArgumentsHost } from '@nestjs/graphql';
+import { GqlArgumentsHost } from '@nestjs/graphql';
 import { ApolloError } from 'apollo-server-core';
+import { ContextType } from '@nestjs/common/interfaces';
 
 @Catch()
 export class GraphQLExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GraphQLExceptionFilter.name);
 
   catch(exception: unknown, host: ArgumentsHost) {
-    // const gqlHost = GqlArgumentsHost.create(host);
+    // const contextType = host.getType() as ContextType;
+
+    // Only apply if context is GraphQL
+    if (host.getType<string>() !== 'graphql') {
+      throw exception;
+    }
+
+    const gqlHost = GqlArgumentsHost.create(host);
 
     this.logger.error('GraphQL Exception:', exception);
 
     if (exception instanceof HttpException) {
       const response = exception.getResponse();
       const message = (response as any)?.message || exception.message;
-      throw new ApolloError(message, exception.getStatus().toString());
+      return new ApolloError(message, exception.getStatus().toString());
     }
 
     if (exception instanceof Error) {
-      throw new ApolloError(exception.message);
+      return new ApolloError(exception.message);
     }
 
-    throw new ApolloError('Unknown error occurred');
+    return new ApolloError('Unknown error occurred');
   }
 }
